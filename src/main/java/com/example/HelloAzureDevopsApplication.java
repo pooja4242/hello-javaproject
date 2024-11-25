22fb842728b07f120b9f4076b76144726f6fec9d
pooja4242 <pooja42848@gmail.com>
2024-11-25 12:31:41 +0530

    first commit

---
+++ src/main/java/com/example/HelloAzureDevopsApplication.java
@@ -0,0 +1,13 @@
+package com.example;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HelloAzureDevopsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(HelloAzureDevopsApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/HelloAzureDevopsApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HelloAzureDevopsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
